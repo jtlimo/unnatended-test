efb790f9fe5f1971bd15b9c2a29362dc614a73bc
Jessica Teixeira de Lima Fileto <jtl.limao@gmail.com>
2021-04-19 20:42:16 -0300

    chore: create scripts to run tests and start server

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Server running at http://localhost:9091")
+	fmt.Println("Ctrl+C or Ctrl+D to quit")
 	log.Fatal((http.ListenAndServe(":9091", nil)))
 }
