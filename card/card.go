9dcb26df327d6512dea19c448ea3e5362f008c63
Jessica Teixeira de Lima Fileto <jtl.limao@gmail.com>
2021-04-21 14:34:06 -0300

    feat: create card deck with determined order

--- card/card.go
+++ card/card.go
@@ -2,12 +2,14 @@ package card
 
 import (
 	"errors"
+	"sort"
 )
 
 type Card struct {
 	Value string `json:"value"`
 	Suit  string `json:"suit"`
 	Code  string `json:"code"`
+	Order int    `json:"order"`
 }
 
 type Carder interface {
@@ -15,58 +17,58 @@ type Carder interface {
 }
 
 var StandardCards = map[string]Card{
-	"AC":  {Value: "ACE", Suit: "CLUBS", Code: "AC"},
-	"2C":  {Value: "2", Suit: "CLUBS", Code: "2C"},
-	"3C":  {Value: "3", Suit: "CLUBS", Code: "3C"},
-	"4C":  {Value: "4", Suit: "CLUBS", Code: "4C"},
-	"5C":  {Value: "5", Suit: "CLUBS", Code: "5C"},
-	"6C":  {Value: "6", Suit: "CLUBS", Code: "6C"},
-	"7C":  {Value: "7", Suit: "CLUBS", Code: "7C"},
-	"8C":  {Value: "8", Suit: "CLUBS", Code: "8C"},
-	"9C":  {Value: "9", Suit: "CLUBS", Code: "9C"},
-	"10C": {Value: "10", Suit: "CLUBS", Code: "10C"},
-	"JC":  {Value: "JACK", Suit: "CLUBS", Code: "JC"},
-	"QC":  {Value: "QUEEN", Suit: "CLUBS", Code: "QC"},
-	"KC":  {Value: "KING", Suit: "CLUBS", Code: "KC"},
-	"AD":  {Value: "ACE", Suit: "DIAMONDS", Code: "AD"},
-	"2D":  {Value: "2", Suit: "DIAMONDS", Code: "2D"},
-	"3D":  {Value: "3", Suit: "DIAMONDS", Code: "3D"},
-	"4D":  {Value: "4", Suit: "DIAMONDS", Code: "4D"},
-	"5D":  {Value: "5", Suit: "DIAMONDS", Code: "5D"},
-	"6D":  {Value: "6", Suit: "DIAMONDS", Code: "6D"},
-	"7D":  {Value: "7", Suit: "DIAMONDS", Code: "7D"},
-	"8D":  {Value: "8", Suit: "DIAMONDS", Code: "8D"},
-	"9D":  {Value: "9", Suit: "DIAMONDS", Code: "9D"},
-	"10D": {Value: "10", Suit: "DIAMONDS", Code: "10D"},
-	"JD":  {Value: "JACK", Suit: "DIAMONDS", Code: "JD"},
-	"QD":  {Value: "QUEEN", Suit: "DIAMONDS", Code: "QD"},
-	"KD":  {Value: "KING", Suit: "DIAMONDS", Code: "KD"},
-	"AH":  {Value: "ACE", Suit: "HEARTS", Code: "AH"},
-	"2H":  {Value: "2", Suit: "HEARTS", Code: "2H"},
-	"3H":  {Value: "3", Suit: "HEARTS", Code: "3H"},
-	"4H":  {Value: "4", Suit: "HEARTS", Code: "4H"},
-	"5H":  {Value: "5", Suit: "HEARTS", Code: "5H"},
-	"6H":  {Value: "6", Suit: "HEARTS", Code: "6H"},
-	"7H":  {Value: "7", Suit: "HEARTS", Code: "7H"},
-	"8H":  {Value: "8", Suit: "HEARTS", Code: "8H"},
-	"9H":  {Value: "9", Suit: "HEARTS", Code: "9H"},
-	"10H": {Value: "10", Suit: "HEARTS", Code: "10H"},
-	"JH":  {Value: "JACK", Suit: "HEARTS", Code: "JH"},
-	"QH":  {Value: "QUEEN", Suit: "HEARTS", Code: "QH"},
-	"KH":  {Value: "KING", Suit: "HEARTS", Code: "KH"},
-	"AS":  {Value: "ACE", Suit: "SPADES", Code: "AS"},
-	"2S":  {Value: "2", Suit: "SPADES", Code: "2S"},
-	"3S":  {Value: "3", Suit: "SPADES", Code: "3S"},
-	"4S":  {Value: "4", Suit: "SPADES", Code: "4S"},
-	"5S":  {Value: "5", Suit: "SPADES", Code: "5S"},
-	"6S":  {Value: "6", Suit: "SPADES", Code: "6S"},
-	"7S":  {Value: "7", Suit: "SPADES", Code: "7S"},
-	"8S":  {Value: "8", Suit: "SPADES", Code: "8S"},
-	"9S":  {Value: "9", Suit: "SPADES", Code: "9S"},
-	"10S": {Value: "10", Suit: "SPADES", Code: "10S"},
-	"JS":  {Value: "JACK", Suit: "SPADES", Code: "JS"},
-	"QS":  {Value: "QUEEN", Suit: "SPADES", Code: "QS"},
-	"KS":  {Value: "KING", Suit: "SPADES", Code: "KS"},
+	"AS":  {Value: "ACE", Suit: "SPADES", Code: "AS", Order: 0},
+	"2S":  {Value: "2", Suit: "SPADES", Code: "2S", Order: 1},
+	"3S":  {Value: "3", Suit: "SPADES", Code: "3S", Order: 2},
+	"4S":  {Value: "4", Suit: "SPADES", Code: "4S", Order: 3},
+	"5S":  {Value: "5", Suit: "SPADES", Code: "5S", Order: 4},
+	"6S":  {Value: "6", Suit: "SPADES", Code: "6S", Order: 5},
+	"7S":  {Value: "7", Suit: "SPADES", Code: "7S", Order: 6},
+	"8S":  {Value: "8", Suit: "SPADES", Code: "8S", Order: 7},
+	"9S":  {Value: "9", Suit: "SPADES", Code: "9S", Order: 8},
+	"10S": {Value: "10", Suit: "SPADES", Code: "10S", Order: 9},
+	"JS":  {Value: "JACK", Suit: "SPADES", Code: "JS", Order: 10},
+	"QS":  {Value: "QUEEN", Suit: "SPADES", Code: "QS", Order: 11},
+	"KS":  {Value: "KING", Suit: "SPADES", Code: "KS", Order: 12},
+	"AD":  {Value: "ACE", Suit: "DIAMONDS", Code: "AD", Order: 13},
+	"2D":  {Value: "2", Suit: "DIAMONDS", Code: "2D", Order: 14},
+	"3D":  {Value: "3", Suit: "DIAMONDS", Code: "3D", Order: 15},
+	"4D":  {Value: "4", Suit: "DIAMONDS", Code: "4D", Order: 16},
+	"5D":  {Value: "5", Suit: "DIAMONDS", Code: "5D", Order: 17},
+	"6D":  {Value: "6", Suit: "DIAMONDS", Code: "6D", Order: 18},
+	"7D":  {Value: "7", Suit: "DIAMONDS", Code: "7D", Order: 19},
+	"8D":  {Value: "8", Suit: "DIAMONDS", Code: "8D", Order: 20},
+	"9D":  {Value: "9", Suit: "DIAMONDS", Code: "9D", Order: 21},
+	"10D": {Value: "10", Suit: "DIAMONDS", Code: "10D", Order: 22},
+	"JD":  {Value: "JACK", Suit: "DIAMONDS", Code: "JD", Order: 23},
+	"QD":  {Value: "QUEEN", Suit: "DIAMONDS", Code: "QD", Order: 24},
+	"KD":  {Value: "KING", Suit: "DIAMONDS", Code: "KD", Order: 25},
+	"AC":  {Value: "ACE", Suit: "CLUBS", Code: "AC", Order: 26},
+	"2C":  {Value: "2", Suit: "CLUBS", Code: "2C", Order: 27},
+	"3C":  {Value: "3", Suit: "CLUBS", Code: "3C", Order: 28},
+	"4C":  {Value: "4", Suit: "CLUBS", Code: "4C", Order: 29},
+	"5C":  {Value: "5", Suit: "CLUBS", Code: "5C", Order: 30},
+	"6C":  {Value: "6", Suit: "CLUBS", Code: "6C", Order: 31},
+	"7C":  {Value: "7", Suit: "CLUBS", Code: "7C", Order: 32},
+	"8C":  {Value: "8", Suit: "CLUBS", Code: "8C", Order: 33},
+	"9C":  {Value: "9", Suit: "CLUBS", Code: "9C", Order: 34},
+	"10C": {Value: "10", Suit: "CLUBS", Code: "10C", Order: 35},
+	"JC":  {Value: "JACK", Suit: "CLUBS", Code: "JC", Order: 36},
+	"QC":  {Value: "QUEEN", Suit: "CLUBS", Code: "QC", Order: 37},
+	"KC":  {Value: "KING", Suit: "CLUBS", Code: "KC", Order: 38},
+	"AH":  {Value: "ACE", Suit: "HEARTS", Code: "AH", Order: 39},
+	"2H":  {Value: "2", Suit: "HEARTS", Code: "2H", Order: 40},
+	"3H":  {Value: "3", Suit: "HEARTS", Code: "3H", Order: 41},
+	"4H":  {Value: "4", Suit: "HEARTS", Code: "4H", Order: 42},
+	"5H":  {Value: "5", Suit: "HEARTS", Code: "5H", Order: 43},
+	"6H":  {Value: "6", Suit: "HEARTS", Code: "6H", Order: 44},
+	"7H":  {Value: "7", Suit: "HEARTS", Code: "7H", Order: 45},
+	"8H":  {Value: "8", Suit: "HEARTS", Code: "8H", Order: 46},
+	"9H":  {Value: "9", Suit: "HEARTS", Code: "9H", Order: 47},
+	"10H": {Value: "10", Suit: "HEARTS", Code: "10H", Order: 48},
+	"JH":  {Value: "JACK", Suit: "HEARTS", Code: "JH", Order: 49},
+	"QH":  {Value: "QUEEN", Suit: "HEARTS", Code: "QH", Order: 50},
+	"KH":  {Value: "KING", Suit: "HEARTS", Code: "KH", Order: 51},
 }
 var StandardCardsCodes = getStandardCardCodes()
 
@@ -100,7 +102,10 @@ func buildCardByCode(code string) []Card {
 			Value: matchCard.Value,
 			Suit:  matchCard.Suit,
 			Code:  code,
+			Order: matchCard.Order,
 		})
+
+	sort.SliceStable(cards, func(i, j int) bool { return cards[i].Order < cards[j].Order })
 	return cards
 
 }
--- deck/deck_test.go
+++ deck/deck_test.go
@@ -2,9 +2,66 @@ package deck
 
 import (
 	"testing"
+	"unnantended/card"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var old = GenerateNewUUID
+var expectedCards = []card.Card{
+	{Value: "ACE", Suit: "SPADES", Code: "AS", Order: 0},
+	{Value: "2", Suit: "SPADES", Code: "2S", Order: 1},
+	{Value: "3", Suit: "SPADES", Code: "3S", Order: 2},
+	{Value: "4", Suit: "SPADES", Code: "4S", Order: 3},
+	{Value: "5", Suit: "SPADES", Code: "5S", Order: 4},
+	{Value: "6", Suit: "SPADES", Code: "6S", Order: 5},
+	{Value: "7", Suit: "SPADES", Code: "7S", Order: 6},
+	{Value: "8", Suit: "SPADES", Code: "8S", Order: 7},
+	{Value: "9", Suit: "SPADES", Code: "9S", Order: 8},
+	{Value: "10", Suit: "SPADES", Code: "10S", Order: 9},
+	{Value: "JACK", Suit: "SPADES", Code: "JS", Order: 10},
+	{Value: "QUEEN", Suit: "SPADES", Code: "QS", Order: 11},
+	{Value: "KING", Suit: "SPADES", Code: "KS", Order: 12},
+	{Value: "ACE", Suit: "DIAMONDS", Code: "AD", Order: 13},
+	{Value: "2", Suit: "DIAMONDS", Code: "2D", Order: 14},
+	{Value: "3", Suit: "DIAMONDS", Code: "3D", Order: 15},
+	{Value: "4", Suit: "DIAMONDS", Code: "4D", Order: 16},
+	{Value: "5", Suit: "DIAMONDS", Code: "5D", Order: 17},
+	{Value: "6", Suit: "DIAMONDS", Code: "6D", Order: 18},
+	{Value: "7", Suit: "DIAMONDS", Code: "7D", Order: 19},
+	{Value: "8", Suit: "DIAMONDS", Code: "8D", Order: 20},
+	{Value: "9", Suit: "DIAMONDS", Code: "9D", Order: 21},
+	{Value: "10", Suit: "DIAMONDS", Code: "10D", Order: 22},
+	{Value: "JACK", Suit: "DIAMONDS", Code: "JD", Order: 23},
+	{Value: "QUEEN", Suit: "DIAMONDS", Code: "QD", Order: 24},
+	{Value: "KING", Suit: "DIAMONDS", Code: "KD", Order: 25},
+	{Value: "ACE", Suit: "CLUBS", Code: "AC", Order: 26},
+	{Value: "2", Suit: "CLUBS", Code: "2C", Order: 27},
+	{Value: "3", Suit: "CLUBS", Code: "3C", Order: 28},
+	{Value: "4", Suit: "CLUBS", Code: "4C", Order: 29},
+	{Value: "5", Suit: "CLUBS", Code: "5C", Order: 30},
+	{Value: "6", Suit: "CLUBS", Code: "6C", Order: 31},
+	{Value: "7", Suit: "CLUBS", Code: "7C", Order: 32},
+	{Value: "8", Suit: "CLUBS", Code: "8C", Order: 33},
+	{Value: "9", Suit: "CLUBS", Code: "9C", Order: 34},
+	{Value: "10", Suit: "CLUBS", Code: "10C", Order: 35},
+	{Value: "JACK", Suit: "CLUBS", Code: "JC", Order: 36},
+	{Value: "QUEEN", Suit: "CLUBS", Code: "QC", Order: 37},
+	{Value: "KING", Suit: "CLUBS", Code: "KC", Order: 38},
+	{Value: "ACE", Suit: "HEARTS", Code: "AH", Order: 39},
+	{Value: "2", Suit: "HEARTS", Code: "2H", Order: 40},
+	{Value: "3", Suit: "HEARTS", Code: "3H", Order: 41},
+	{Value: "4", Suit: "HEARTS", Code: "4H", Order: 42},
+	{Value: "5", Suit: "HEARTS", Code: "5H", Order: 43},
+	{Value: "6", Suit: "HEARTS", Code: "6H", Order: 44},
+	{Value: "7", Suit: "HEARTS", Code: "7H", Order: 45},
+	{Value: "8", Suit: "HEARTS", Code: "8H", Order: 46},
+	{Value: "9", Suit: "HEARTS", Code: "9H", Order: 47},
+	{Value: "10", Suit: "HEARTS", Code: "10H", Order: 48},
+	{Value: "JACK", Suit: "HEARTS", Code: "JH", Order: 49},
+	{Value: "QUEEN", Suit: "HEARTS", Code: "QH", Order: 50},
+	{Value: "KING", Suit: "HEARTS", Code: "KH", Order: 51},
+}
 
 func TestBuildStandardDeck(t *testing.T) {
 	defer func() { GenerateNewUUID = old }()
@@ -14,6 +71,7 @@ func TestBuildStandardDeck(t *testing.T) {
 
 	defaultDeck := NewDeck()
 
+	assert.Equal(t, expectedCards, defaultDeck[GenerateNewUUID()].Cards)
 	assertDeckLength(t, defaultDeck[GenerateNewUUID()], 52)
 }
 
