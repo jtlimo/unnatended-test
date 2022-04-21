83f6e99483555c9db85d8a21c461dce6a6a42cdd
Jessica Teixeira de Lima Fileto <jessica.l.fileto@protonmail.com>
2022-04-21 20:23:54 -0300

    feat: add create and open deck features

--- server/server.go
+++ server/server.go
@@ -1,10 +1,15 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
+	"strings"
 	"unattended-test/database"
+	"unattended-test/deck"
+	"unattended-test/server/dto"
 )
 
 type Server struct {
@@ -21,17 +26,62 @@ func (s *Server) Setup() *mux.Router {
 }
 
 func (s *Server) createRoutes() {
-	s.Router.HandleFunc("/deck", createDeck).Methods("POST").Queries("cards", "{cards}")
-	http.Handle("/", s.Router)
+	s.Router.HandleFunc("/deck", s.createDeck).Methods("POST")
+	s.Router.HandleFunc("/deck/{uuid}", s.openDeck).Methods("GET")
 }
 
 func (s *Server) initializeDB() {
 	s.Db = database.Init()
 }
 
-func createDeck(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+func (s *Server) createDeck(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	cards := strings.Split(params.Get("cards"), ",")
+	shuffleStr := params.Get("shuffle")
+	shuffle := false
+	var err error
 
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Deck created: %v\n", vars["cards"])
+	if len(shuffleStr) > 0 {
+		shuffle, err = strconv.ParseBool(shuffleStr)
+	}
+
+	if err != nil {
+		w.WriteHeader(400)
+		return
+	}
+
+	d, err := deck.NewDeck(cards, shuffle)
+
+	if err != nil {
+		w.WriteHeader(400)
+	}
+	s.Db.Insert(d)
+	payload, err := json.Marshal(dto.ToDeck(d))
+
+	if err != nil {
+		fmt.Println("unable to encode json")
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
+}
+
+func (s *Server) openDeck(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	uuid := vars["uuid"]
+
+	d, err := s.Db.GetByDeckId(uuid)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	payload, err := json.Marshal(dto.ToOpenDeck(d))
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
 }
--- server/server_test.go
+++ server/server_test.go
@@ -1,51 +1,160 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
+	"unattended-test/card"
 	"unattended-test/deck"
+	"unattended-test/server/dto"
 )
 
-var expectedDeck = deck.Deck{
-	Id:        "7dd13273-fabb-4223-9df6-9646c9473880",
-	Shuffled:  false,
-	Remaining: 52,
-}
 var s = Server{}
 
 func TestCreateDeck(t *testing.T) {
 	t.Run("create a new standard Deck", func(t *testing.T) {
+		expectedDeck := dto.DeckDTO{
+			Id:        "7dd13273-fabb-4223-9df6-9646c9473880",
+			Shuffled:  false,
+			Remaining: 52,
+		}
 		router := s.Setup()
 		localServer := httptest.NewServer(router)
 		defer localServer.Close()
-
 		var old = deck.GenerateNewUUID
 		defer func() { deck.GenerateNewUUID = old }()
 		deck.GenerateNewUUID = func() string {
 			return "7dd13273-fabb-4223-9df6-9646c9473880"
 		}
-		request := create()
+		request := create("false")
 		res := executeRequest(request)
 
-		d := s.Db.Get()
-		if assert.NotNil(t, d) {
-			assert.Equal(t, expectedDeck, d)
+		payload, _ := ioutil.ReadAll(res.Body)
+
+		var jsonData dto.DeckDTO
+		json.Unmarshal(payload, &jsonData)
+
+		if assert.NotNil(t, jsonData) {
+			assert.Equal(t, expectedDeck, jsonData)
+		}
+		assertStatus(t, res.Code, 200)
+	})
+
+	t.Run("create a new custom Deck", func(t *testing.T) {
+		expectedDeck := dto.DeckDTO{
+			Id:        "7dd13273-fabb-4223-9df6-9646c9473881",
+			Shuffled:  false,
+			Remaining: 3,
+		}
+		router := s.Setup()
+		localServer := httptest.NewServer(router)
+		defer localServer.Close()
+		var old = deck.GenerateNewUUID
+		defer func() { deck.GenerateNewUUID = old }()
+		deck.GenerateNewUUID = func() string {
+			return "7dd13273-fabb-4223-9df6-9646c9473881"
+		}
+		request := create("false", "AS,2S,JS")
+		res := executeRequest(request)
+
+		payload, _ := ioutil.ReadAll(res.Body)
+
+		var jsonData dto.DeckDTO
+		json.Unmarshal(payload, &jsonData)
+
+		if assert.NotNil(t, jsonData) {
+			assert.Equal(t, expectedDeck, jsonData)
+		}
+		assertStatus(t, res.Code, 200)
+	})
+
+	t.Run("create a shuffled Deck", func(t *testing.T) {
+		expectedDeck := dto.DeckDTO{
+			Id:        "7dd13273-fabb-4223-9df6-9646c9473882",
+			Shuffled:  true,
+			Remaining: 3,
+		}
+		router := s.Setup()
+		localServer := httptest.NewServer(router)
+		defer localServer.Close()
+		var old = deck.GenerateNewUUID
+		defer func() { deck.GenerateNewUUID = old }()
+		deck.GenerateNewUUID = func() string {
+			return "7dd13273-fabb-4223-9df6-9646c9473882"
+		}
+		request := create("true", "AS,2S,JS")
+		res := executeRequest(request)
+
+		payload, _ := ioutil.ReadAll(res.Body)
+
+		var jsonData dto.DeckDTO
+		json.Unmarshal(payload, &jsonData)
+
+		if assert.NotNil(t, jsonData) {
+			assert.Equal(t, expectedDeck, jsonData)
 		}
 		assertStatus(t, res.Code, 200)
 	})
 }
 
-func create(cards ...string) *http.Request {
-	var req, _ *http.Request
-	if len(cards) > 0 {
-		req, _ = http.NewRequest(http.MethodPost, fmt.Sprintf("/deck?cards=%s", cards), nil)
-	} else {
-		req, _ = http.NewRequest(http.MethodPost, "/deck", nil)
+func TestOpenDeck(t *testing.T) {
+	t.Run("open an existent deck", func(t *testing.T) {
+		cards, _ := card.NewCard([]string{"AS", "JD", "QH"})
+		expectedDeck := dto.OpenDeckDTO{
+			DeckDTO: dto.DeckDTO{
+				Id:        "7dd13273-fabb-4223-9df6-9646c9473890",
+				Shuffled:  false,
+				Remaining: 3,
+			},
+			CardDTO: dto.ToCard(cards),
+		}
+		router := s.Setup()
+		localServer := httptest.NewServer(router)
+		defer localServer.Close()
+		var old = deck.GenerateNewUUID
+		defer func() { deck.GenerateNewUUID = old }()
+		deck.GenerateNewUUID = func() string {
+			return "7dd13273-fabb-4223-9df6-9646c9473890"
+		}
+		request := create("false", "AS,JD,QH")
+		executeRequest(request)
 
+		openRequest := open("7dd13273-fabb-4223-9df6-9646c9473890")
+
+		res := executeRequest(openRequest)
+
+		payload, _ := ioutil.ReadAll(res.Body)
+
+		var jsonData dto.OpenDeckDTO
+		json.Unmarshal(payload, &jsonData)
+
+		if assert.NotNil(t, jsonData) {
+			assert.Equal(t, expectedDeck, jsonData)
+		}
+		assertStatus(t, res.Code, 200)
+	})
+
+}
+
+func create(shuffle string, cards ...string) *http.Request {
+	req, _ := http.NewRequest(http.MethodPost, "/deck", nil)
+	queryParams := url.Values{
+		"cards":   cards,
+		"shuffle": []string{shuffle},
 	}
+	req.URL.RawQuery = queryParams.Encode()
+
+	return req
+}
+
+func open(uuid string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/deck/%s", uuid), nil)
+
 	return req
 }
 
